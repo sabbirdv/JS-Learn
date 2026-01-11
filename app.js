02b2ee721e3255a75bdd3bf8f1387da5a35469ec
sabbirdv <sabbirhasan.dv@gmail.com>
2026-01-11 13:44:19 +0600

    JS

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 function timeBtn(){
-    alert("Did you want to know the time?");
-    alert("Confirm!")
     alert("Are you sure!?")
     document.getElementById("demo").innerHTML=new Date().toLocaleTimeString('en-US');
+}
+function inputTry(){
+    document.getElementById("input").style= "background-color:lightgreen";
 }
