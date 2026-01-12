cc8c777b0c56ae493d1c136783f7fe13dd224a65
sabbirdv <sabbirhasan.dv@gmail.com>
2026-01-12 21:12:32 +0600

    array loop

--- app.js
+++ app.js
@@ -1,5 +1,21 @@
 const test = ["red" , "blue" ,"green" , "orange"];
-test[4] = "purple";
-test[5] = "pink";
-test[6] = "white"
-console.log(test[test.length-1]);
+
+text = "<ul>";
+for(i=0; i < test.length; i++){
+    text+= "<li>" + test[i] + "</li>"
+};
+text += "</ul>";
+document.getElementById("demo").innerHTML=text;
+
+
+const test2 = ["red" , "blue" ,"green" , "orange"];
+
+text2 = "<ul>"
+test.forEach(theFunction);
+text2 += "</ul>";
+
+function theFunction(value){
+    text2 += "<li>" + value + ` color` + "</li>";
+}
+
+document.getElementById("demo2").innerHTML=text2;
