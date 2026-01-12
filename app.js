820fb0b2d5afe973df4820f889adad3822ac384f
sabbirdv <sabbirhasan.dv@gmail.com>
2026-01-12 12:27:31 +0600

    test

--- app.js
+++ app.js
@@ -1,3 +1,8 @@
 let str = "my name is Sabbir hasan.";
 let str2 = `Hi there ${str} I am a student`
 console.log(str2);
+
+let price = 20;
+let vat = 0.26;
+let total = `The price of the product is ${price} & the total price is ${price + vat}`;
+console.log(total);
