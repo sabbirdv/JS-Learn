6e45b30084632158f2fbe8f83f607895ebfd6735
sabbirdv <sabbirhasan.dv@gmail.com>
2026-01-13 15:56:23 +0600

    aray

--- app.js
+++ app.js
@@ -1,21 +1,47 @@
-const test = ["red" , "blue" ,"green" , "orange"];
+const test1 = ["red" , "blue" ,"green" , "orange" , "black", "white"];
+const test2 = [2,4,25,5,14,8,76,35,24]
 
-text = "<ul>";
-for(i=0; i < test.length; i++){
-    text+= "<li>" + test[i] + "</li>"
-};
-text += "</ul>";
-document.getElementById("demo").innerHTML=text;
-
-
-const test2 = ["red" , "blue" ,"green" , "orange"];
-
-text2 = "<ul>"
-test.forEach(theFunction);
-text2 += "</ul>";
-
-function theFunction(value){
-    text2 += "<li>" + value + ` color` + "</li>";
+function myFunction(value){
+    return value < 30;
 }
 
-document.getElementById("demo2").innerHTML=text2;
+
+const test2New = test2.filter(myFunction);
+
+test2New.sort(function(a,b){
+    return a-b;
+});
+
+
+
+console.log(test2New);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const number1 = [2,3,12,25,6,5,23,4,6];
+
+// number1.sort(function (a,b){
+//     return a-b
+// });
+// console.log(number1);
