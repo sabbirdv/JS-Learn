a685aaabfa820534155ebc1a75c7aee5f7d3ec26
sabbirdv <sabbirhasan.dv@gmail.com>
2026-01-13 21:52:04 +0600

    array

--- app.js
+++ app.js
@@ -1,18 +1,11 @@
 const test1 = ["red" , "blue" ,"green" , "orange" , "black", "white"];
-const test2 = [2,4,25,5,14,8,76,35,24]
+const test2 = [2,4,21,5,10,8]
 
-function myFunction(value){
-    return value < 30;
+const test2New = test2.find(myFunction);
+
+function myFunction(value, index, array){
+    return value > 10 ;
 }
-
-
-const test2New = test2.filter(myFunction);
-
-test2New.sort(function(a,b){
-    return a-b;
-});
-
-
 
 console.log(test2New);
 
