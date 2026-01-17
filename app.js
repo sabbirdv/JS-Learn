aadc340791ce4b2e85287763900e5eff350b7f80
sabbirdv <sabbirhasan.dv@gmail.com>
2026-01-18 00:32:10 +0600

    b

--- app.js
+++ app.js
@@ -1,24 +1,13 @@
-const d = new Date();
-
-const months = ["January", "February", "March", "April", "May" , "June", "July", "August", "September", "October", "November", "December"];
-
-// months[0]="sunday"
-// months.splice(0,1,"Sunday");
-console.log(months);
-document.getElementById("demo").innerHTML= months[d.getMonth()]  + d.getDate() ;
+let text = "";
+const today = new Date();
+const someday = new Date();
 
 
+if (someday > today){
+    text = "Today is Before Someday";
+} else {
+    text = "Today is after Someday";
+};
 
 
-
-
-
-
-
-
-// const number1 = [2,3,12,25,6,5,23,4,6];
-
-// number1.sort(function (a,b){
-//     return a-b
-// });
-// console.log(number1);
+console.log(text);
