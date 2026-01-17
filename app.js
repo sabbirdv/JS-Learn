2d0d8d7bd07ecbdd81e5f926953542d268582820
sabbirdv <sabbirhasan.dv@gmail.com>
2026-01-18 01:45:04 +0600

    math

--- app.js
+++ app.js
@@ -1,13 +1,19 @@
-let text = "";
-const today = new Date();
-const someday = new Date();
+
+const a = [ 3 , 4 ,5, 2, 67 ,32 ,12 ,54, 3,54 ,34]
+let b = a;
 
 
-if (someday > today){
-    text = "Today is Before Someday";
-} else {
-    text = "Today is after Someday";
-};
-
-
-console.log(text);
+document.getElementById("demo").innerHTML= 
+  Math.round(4.5) + " , " 
++ Math.ceil(4.2) + " , " 
++ Math.floor(4.7) + " , " 
++ Math.trunc(4.572) + " , " 
++ Math.sign(-5) + " , " 
++ Math.pow(4, 2) + " , " 
++ Math.sqrt(16) + " , " 
++ Math.abs(-37) + " , "
++ Math.min(2,3, 5, 64 , 12, 34 ,4 ,5 ,7,43, 23) + " , "
++ Math.max(2,3, 5, 64 , 12, 34 ,4 ,5 ,7,43, 23) + " , "
++ Math.min(...a) + " , "
++ Math.max(...a) + " , "
++ Math.floor(Math.random(...a) * (25 - 20 + 1) + 20) ;
